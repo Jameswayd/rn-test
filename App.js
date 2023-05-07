a1694a9675dc6515c27eb9ba4c64e94e3ce710fb
Jameswayd <a123856426@gmail.com>
2023-05-07 14:45:37 +0800

    restore files

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './HomeScreen.js';
+import DetailScreen from './DetailScreen';
+
+const Tab = createBottomTabNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={DetailScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
